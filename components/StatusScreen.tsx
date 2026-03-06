ff54b2dbe57e60088dcf3b028163e9b51023f938
Deyvid Dimitrov <davida1991@gmail.com>
2026-03-06 08:25:31 +0200

    UI: Move Facebook icon to top-left to avoid overlap with Donate button

--- components/StatusScreen.tsx
+++ components/StatusScreen.tsx
@@ -139,7 +139,7 @@ export const StatusScreen: React.FC<StatusScreenProps> = ({
       </div>
 
       {/* Social Links & Other Fixed Controls */}
-      <div className="fixed top-6 right-6 z-50 flex items-center gap-3">
+      <div className="fixed top-6 left-20 z-50 flex items-center gap-3">
         <a
           href="https://www.facebook.com/profile.php?id=61586612323239"
           target="_blank"
